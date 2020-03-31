8778d2fe6fc3e472290237a176aa8760e5136c58
tarek1500 <tarek_mohammed2@yahoo.com>
2020-03-31 21:11:54 +0200

    Auth Controller
    Setup web Auth logic.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('welcome'); })->name('home');
+
+Auth::routes([
+	'register' => false
+]);
