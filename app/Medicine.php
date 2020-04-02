9ba2d734ad440b88eff3b76613f26c458463b92f
sarahIbraheem <sarahmuhammad3377@gmail.com>
2020-04-02 23:46:44 +0200

    Add Medicine model with relations

---
+++ app/Medicine.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Medicine extends Model
+{
+    protected $fillable = [
+        'name',
+        'type',
+    ];
+    
+    // The pharmacies that contain the medicine.
+
+    public function pharmacies()
+    {   //many to many 
+
+        return $this->belongsToMany(Pharmacy::class,'medicine_pharmacies');
+    }
+    
+    /* is commented until Order model is maked
+
+     // The orders that contain the medicine.
+
+     public function orders()
+     {   //many to many
+
+         return $this->belongsToMany(Order::class,'medicine_orders');
+     }
+     */
+}
---
+++ database/migrations/2020_04_01_122709_create_medicines_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMedicinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicines', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicines');
+    }
+}
