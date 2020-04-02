aa11c0d06e2e97c560a6d065f552f5992748e442
sarahIbraheem <sarahmuhammad3377@gmail.com>
2020-04-02 23:47:18 +0200

    Add Pharmacy model with relations

---
+++ app/Pharmacy.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pharmacy extends Model
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'national_id',
+        'avatar_image',
+        'priority',
+        'area_id',
+    ];
+    protected $hidden = [
+        'password',
+    ];
+
+    // is commented until Area model is maked
+    /* public function area()
+    {
+         return $this->belongsTo('App\Area');
+    } */
+
+      
+    // The medicines that belong to the pharmacy.
+    
+    public function medicines()
+    {  //many to many relation
+        return $this->belongsToMany(Medicine::class,'medicine_pharmacies');
+    }
+
+
+}
---
+++ database/migrations/2020_04_01_122612_create_pharmacies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePharmaciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pharmacies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->unsignedBigInteger('national_id')->unique();
+            $table->string('avatar_image')->default('default.jpg');
+            $table->unsignedBigInteger('priority');
+            $table->unsignedBigInteger('area_id');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pharmacies');
+    }
+}
