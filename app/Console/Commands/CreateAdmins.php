b64472738e16b7e02cfecd37cc2bd473c9962e18
sarahIbraheem <sarahmuhammad3377@gmail.com>
2020-04-02 23:56:56 +0200

    Add create admin command to create other admins through console command

---
+++ app/Console/Commands/CreateAdmins.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use App\Admin;
+
+class CreateAdmins extends Command
+{
+   
+     /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+
+    protected $signature = 'create:admin {--email=} {--password=}';
+    /* example for command :
+        php artisan create:admin --email=admin2@admin.com --password=123456
+    */
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+
+    /* this description will show when user use php artisan command*/
+    protected $description = 'Create Admin with Email and Password';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {   
+        Admin::create([
+            'email' => $this->option('email'), //$this->option('email')->the value inserted by user
+            'password' =>Hash::make($this->option('password')),
+        ]);
+
+    }
+}
