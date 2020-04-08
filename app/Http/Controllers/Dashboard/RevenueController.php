8f800689a4b12929d1dbfe2d8f9ee49530e72c71
sarahIbraheem <sarahmuhammad3377@gmail.com>
2020-04-08 12:11:10 +0200

    Revenue tab with datatable and ajax

--- app/Http/Controllers/Dashboard/RevenueController.php
+++ app/Http/Controllers/Dashboard/RevenueController.php
@@ -20,18 +20,19 @@ class RevenueController extends Controller
         $pharmacies_total=0;
         $pharmacies = Pharmacy::all();
         foreach($pharmacies as $pharmacy)
-        {$orders=Order::where('pharamcy_id',$pharmacy->id)->get();
-         $TotalOrders=$orders->count();
-         $TotalRevenue=$orders->sum('total_price');
-         $pharmacies_total=$pharmacies_total+$TotalRevenue;
-         array_push($rev,
-         [  'avatar_image'=> $pharmacy->avatar_image,
-            'name'=> $pharmacy->name,
-            'TotalRevenue'=> $TotalRevenue,
-            'TotalOrders'=> $TotalOrders,
-         ]);
-        
-        }
+            {
+                $orders=Order::where('pharamcy_id',$pharmacy->id)->get();
+                $TotalOrders=$orders->count();
+                $TotalRevenue=$orders->sum('total_price');
+                $pharmacies_total=$pharmacies_total+$TotalRevenue;
+                array_push($rev,
+                [  'avatar_image'=> $pharmacy->avatar_image,
+                    'name'=> $pharmacy->name,
+                    'TotalRevenue'=> $TotalRevenue,
+                    'TotalOrders'=> $TotalOrders,
+                ]);
+            
+            }
         if(request()->ajax())
         {
             return datatables()->of($rev)->make(true);
