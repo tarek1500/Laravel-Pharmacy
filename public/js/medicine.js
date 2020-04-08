28f21800f5aa2aae359bace4c952defde1d53bcf
MohamedGaber-intake40 <gaberm514@gmail.com>
2020-04-08 03:09:32 +0200

    implement edit and update order methods create edit view for order add total price and add or remove btns  to create view custom style js script to add new row medicine or delete it

--- public/js/medicine.js
+++ public/js/medicine.js
@@ -1,52 +1,81 @@
-$('.medicineContainer').delegate('button[type=button]', 'click', addMedRow);
-
+$('.medicineContainer').delegate('button.add', 'click', addMedRow);
+$('.medicineContainer').delegate('button.delete', 'click', deleteMedRow);
+$('.medicineContainer').delegate('.price', 'input', updateTotalPrice);
+$('.medicineContainer').delegate('.quantity', 'input', updateTotalPrice);
+const totalPriceSpan = document.querySelector('#totalPrice')
+document.onload = updateTotalPrice()
 function addMedRow() {
-    const medPriceContainer = $('#medPriceContainer').clone()
-    const addMedBtnContainer = $('#addMedBtnContainer').clone()
-    const medQuantityContainer = $('#medQuanityContainer').clone()
+    const medPriceContainer = $('.medPriceContainer').first().clone().removeClass('medPriceContainer')
+    const addMedBtnContainer = $('.addMedBtnContainer').first().clone().removeClass('addMedBtnContainer')
+    const medQuantityContainer = $('.medQuanityContainer').first().clone().removeClass('medQuanityContainer')
+    medQuantityContainer.children('input').val('').addClass('quantity')
+    medPriceContainer.children('input').val('')
+    medPriceContainer.children('input').val('').addClass('price')
     const medData = $('.medData').clone().removeClass('d-none medData')
     const typeData = $('.typeData').clone().removeClass('d-none typeData')
-    
+    const medicineContainer = $('.medicineContainer').last()
+    const medicineRow=$('<div></div>').addClass('row')
+
     $('<div></div>').addClass('col-4')
         .append('<label>Medicine Name</label>')
         .append(medData)
-        .appendTo('.medicineRow')
-    medData.select2({
-        tags: true,
-        placeholder: "Select a medicine name",
-        allowClear:true
+        .appendTo(medicineRow)
 
-    })
-    
     $('<div></div>').addClass('col-3')
-    .append('<label>Medicine Type</label>')
-    .append(typeData)
-    .appendTo('.medicineRow')
-    typeData.select2({
-        tags: true,
-        placeholder: "Select medicine type",
-        allowClear:true
+        .append('<label>Medicine Type</label>')
+        .append(typeData)
+        .appendTo(medicineRow)
 
-    })
+        medQuantityContainer.appendTo(medicineRow)
+        medPriceContainer.appendTo(medicineRow)
+        addMedBtnContainer.appendTo(medicineRow)
+        medicineRow.appendTo(medicineContainer)
+        
+        medData.select2({
+            tags: true,
+            placeholder: "Select a medicine name",
+            allowClear: true
+            
+        })
+        typeData.select2({
+            tags: true,
+            placeholder: "Select medicine type",
+            allowClear: true
+    
+        })
 
-    medPriceContainer.appendTo('.medicineRow')
-    medQuantityContainer.appendTo('.medicineRow')
-    addMedBtnContainer.appendTo('.medicineRow')
+    
 }
 
 
 $('#userSelect').select2({
     placeholder: "Select user name ",
-    allowClear:true
+    allowClear: true
 
 })
 $('.medicineNameSelect').select2({
     tags: true,
     placeholder: "Select a medicine name",
-    allowClear:true
+    allowClear: true
 })
 $('.medicineTypeSelect').select2({
     tags: true,
     placeholder: "Select medicine type",
-    allowClear:true
+    allowClear: true
 })
+
+
+function updateTotalPrice() {
+    const quantities=document.querySelectorAll('.quantity')
+    const prices=document.querySelectorAll('.price')
+    let totalPrice = 0;
+    for (let i = 0; i < quantities.length; i++) {
+        totalPrice += +quantities[i].value * +prices[i].value;
+    }
+    totalPriceSpan.innerText = totalPrice + ' $'
+}
+
+function deleteMedRow(e)
+{
+    $(this).parent().parent().remove()
+}
