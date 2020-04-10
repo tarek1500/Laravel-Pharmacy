ce6da1466b12e73877c824555044d11bc7bda772
sarahIbraheem <sarahmuhammad3377@gmail.com>
2020-04-10 23:31:48 +0200

    add assign order schedule command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+
 class Kernel extends ConsoleKernel
 {
     /**
@@ -13,7 +14,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        'App\Console\Commands\NotifyLastLogin',
+        'App\Console\Commands\AssignOrders',
     ];
 
     /**
@@ -26,6 +28,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('notify:last_login')->daily();
         $schedule->command('notify:users')->daily();
+        $schedule->command('assign:order')->everyMinute();
     }
 
     /**
